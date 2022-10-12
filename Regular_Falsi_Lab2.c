79d232014e9191e10aee67d58caa70e3a70d193c
Diya <diyag899@gmail.com>
2022-10-12 14:55:09 +0530

    Regula Falsi

---
+++ Regular_Falsi_Lab2.c
@@ -0,0 +1,46 @@
+/* NAME - DIYA GOYAL
+   SECTION - H
+   ROLLNO - 52
+   UNIVERSITY ROLLNO - 2016741 
+*/ 
+#include<stdio.h>
+#include<math.h>
+float f(float x)
+{
+    return x*x*x -2*x - 5;
+}
+int main()
+{
+    int count = 1;
+    float x1,x2;
+    int i;
+    printf("Name - Diya Goyal\nSection - H\nRollno - 52\n");
+    printf("Enter 2 valid guesses\n");
+    scanf("%f",&x1);
+    scanf("%f",&x2);
+    printf("Enter the no. of iterations allowed\n");
+    scanf("%d",&i);
+    if(f(x1)*f(x2) >= 0.0){
+        printf("Enter valid guesses");
+    }
+    else
+    {
+        while(count <= i)
+        {
+            float mid = ((x1*f(x2)-x2*f(x1))/(f(x2)-f(x1)));
+            if(f(mid)==0.0)
+            {
+                printf("the root of the equation in the iteration %d %f\n",count,mid);
+                break;
+            }
+            if(f(x1)*f(mid) < 0)
+            x2 = mid;
+            else
+            x1 = mid;
+            printf("the root of the equation in the iteration %d %f\n",count,mid);
+            count++;
+        }
+    }
+
+    return 0;
+}
