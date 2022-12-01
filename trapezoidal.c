907c9cd3029f48a14e24c621f60fb80fee413eba
Diya <diyag899@gmail.com>
2022-12-02 00:09:17 +0530

    Trapezodial

---
+++ trapezoidal.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+float findValueAt(float x)
+{
+    return 1/(1+x*x);
+}
+int main()
+{
+    int n;
+    float i,a,b,sum=0,h;
+    //Input
+    printf("Enter Value of a and b\n");
+    scanf("%f%f",&a,&b);
+    printf("Enter no. of Intervals\n");
+    scanf("%d",&n);
+    h=(b-a)/n;
+    sum = findValueAt(a) +findValueAt(b);
+    for(i=a+h;i<b;i=i+h)
+       sum = sum + 2*findValueAt(i);
+    sum = (h * sum)/2;
+    //Print the Output
+    printf("\nValue of The integral  = %f",sum);    
+    
+}
